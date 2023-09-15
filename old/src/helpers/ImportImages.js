632039ac97538efab1bea1c1adc96f79332cf374
Adonay Douglas <adonay.douglas.profissional@gmail.com>
2023-09-15 17:48:30 -0300

    feat: deletando pasta old

--- old/src/helpers/ImportImages.js
+++
@@ -1,6 +0,0 @@
-// Header Cover
-import headerCover from "./../assets/images/backgrounds/particles-background.webp";
-// Avatar
-import Avatar from "./../assets/images/avatar.png";
-
-export { headerCover, Avatar };
