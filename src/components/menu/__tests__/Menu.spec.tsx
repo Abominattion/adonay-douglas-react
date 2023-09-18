8cac29327e6c45209da1d6721fbfbe7bd5fe11ad
Adonay Douglas <adonay.douglas.profissional@gmail.com>
2023-09-18 18:59:26 -0300

    fix: removendo fução não usado no teste

--- src/components/menu/__tests__/Menu.spec.tsx
+++ src/components/menu/__tests__/Menu.spec.tsx
@@ -1,4 +1,4 @@
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 // Enum
 import { MenuTestIDEnum } from "../enum/MenuTestIDEnum";
